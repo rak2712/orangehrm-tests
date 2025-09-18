5f1134c4067579954f40d93b9f44ecb2bae99924
rak2712 <rakshithhn.1ga14ec416@gmail.com>
2025-09-18 11:45:31 +0530

    first commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class LoginPage:
     def __init__(self, driver):
