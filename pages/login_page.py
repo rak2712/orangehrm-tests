24852a318acb05a414e290f5d063555df2b589b2
rak2712 <rakshithhn.1ga14ec416@gmail.com>
2025-09-18 11:44:22 +0530

    first commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,31 +1,19 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
         self.username_input = (By.NAME, "username")
         self.password_input = (By.NAME, "password")
         self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
         self.error_message = (By.CSS_SELECTOR, ".oxd-alert-content-text")
 
     def login(self, username, password):
-        wait = WebDriverWait(self.driver, 10)
-
-        username_field = wait.until(EC.presence_of_element_located(self.username_input))
-        password_field = wait.until(EC.presence_of_element_located(self.password_input))
-        login_btn = wait.until(EC.element_to_be_clickable(self.login_button))
-
-        username_field.clear()
-        username_field.send_keys(username)
-
-        password_field.clear()
-        password_field.send_keys(password)
-
-        login_btn.click()
+        self.wait.until(EC.presence_of_element_located(self.username_input)).send_keys(username)
+        self.wait.until(EC.presence_of_element_located(self.password_input)).send_keys(password)
+        self.wait.until(EC.element_to_be_clickable(self.login_button)).click()
 
     def get_error_message(self):
-        wait = WebDriverWait(self.driver, 5)
-        return wait.until(EC.presence_of_element_located(self.error_message)).text
+        return self.wait.until(EC.visibility_of_element_located(self.error_message)).text
