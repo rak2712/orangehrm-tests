a39a4424c01f9412a86ac31c0698c22bfd862c00
rak2712 <rakshithhn.1ga14ec416@gmail.com>
2025-09-18 11:32:46 +0530

    first commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver):
@@ -9,9 +10,20 @@ class LoginPage:
         self.error_message = (By.CSS_SELECTOR, ".oxd-alert-content-text")
 
     def login(self, username, password):
-        self.driver.find_element(*self.username_input).send_keys(username)
-        self.driver.find_element(*self.password_input).send_keys(password)
-        self.driver.find_element(*self.login_button).click()
+        wait = WebDriverWait(self.driver, 10)
+
+        username_field = wait.until(EC.presence_of_element_located(self.username_input))
+        password_field = wait.until(EC.presence_of_element_located(self.password_input))
+        login_btn = wait.until(EC.element_to_be_clickable(self.login_button))
+
+        username_field.clear()
+        username_field.send_keys(username)
+
+        password_field.clear()
+        password_field.send_keys(password)
+
+        login_btn.click()
 
     def get_error_message(self):
-        return self.driver.find_element(*self.error_message).text
+        wait = WebDriverWait(self.driver, 5)
+        return wait.until(EC.presence_of_element_located(self.error_message)).text
