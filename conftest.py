0eaa7dac3f625de1828731dc77aef07ae9daf75a
rak2712 <rakshithhn.1ga14ec416@gmail.com>
2025-09-18 11:27:53 +0530

    first commit

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ load_dotenv()
 @pytest.fixture(scope ="session")
 def driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
